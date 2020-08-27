fd663a5e3af88eed76e1a88df912ea6c9d7582eb
min1474 <wkdals1474@naver.com>
2020-08-27 18:09:52 +0900

    no message

---
+++ src/main/java/com/lottomatic/demo/controrller/DemoController.java
@@ -0,0 +1,16 @@
+package com.lottomatic.demo.controrller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
+@RestController
+public class DemoController {
+
+    @GetMapping("/api/hello")
+    public String hello(){
+        return "안녕하세요. 현재 서버시간은 "+new Date() +"입니다. \n";
+    }
+
+}
